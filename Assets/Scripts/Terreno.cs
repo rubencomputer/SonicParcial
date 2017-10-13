b0f0a986bbed1ea0194880c12513d4ef476922a7
Oscar Villarreal <30502136+TheScarred@users.noreply.github.com>
2017-10-12 20:33:20 -0500

    Agregado de arreglos
    Se agregaron otros prefabs + se hicieron los arreglos de bloques que se spawnearan infinitamente

---
+++ Assets/Scripts/Terreno.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terreno : MonoBehaviour {
+
+	public GameObject[] Bloques;
+	public GameObject[] Puentes;
+	public GameObject[] Puenques;
+	public GameObject Vacio;
+	public GameObject[] Obstaculos;
+	public GameObject[] Items;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
