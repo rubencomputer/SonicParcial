5772e54dd26db3b8c6e96400a53364abd2f265fa
Oscar Villarreal <30502136+TheScarred@users.noreply.github.com>
2017-10-13 02:10:26 -0500

    Escena de Prueba
    Se hizo una escena de pruebas y se creo el movimiento del jugador. (escena Test) tambien se probo con spawnear el mismo bloque infinitamente... Falta destruir los que quedan atras.

---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour {
+
+	public Transform jugador;
+	private float velH=0;
+	public int hilera = 0;
+	public bool movimiento;
+	public float velocidad = 20;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		GetComponent<Rigidbody> ().velocity = new Vector3 (velH, 0, velocidad);
+		if ((Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.LeftArrow)) && (hilera>-1) && (movimiento)) {
+			movimiento = false;
+			hilera -= 1;
+			velH = -12.5f;
+			StartCoroutine (detenerMovimientoH());
+		}
+		if ((Input.GetKeyDown (KeyCode.D) || Input.GetKeyDown (KeyCode.RightArrow)) && (hilera<1) && (movimiento)) {
+			movimiento = false;
+			hilera += 1;
+			velH = 12.5f;
+			StartCoroutine (detenerMovimientoH ());
+		}
+	
+	}
+	IEnumerator detenerMovimientoH()
+	{
+		yield return new WaitForSeconds (.25f);
+			velH = 0;
+			movimiento = true;
+	}
+}
--- Assets/Scripts/Terreno.cs
+++ Assets/Scripts/Terreno.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Terreno : MonoBehaviour {
 
+
+public class Terreno : MonoBehaviour {
+	
 	public GameObject[] Bloques;
 	public GameObject[] Puentes;
 	public GameObject[] Puenques;
 	public GameObject Vacio;
 	public GameObject[] Obstaculos;
 	public GameObject[] Items;
+	private float dEntreBloques = 7.6175f;
+	private float escenaZ = 83.7925f;
 
 	// Use this for initialization
 	void Start () {
-		
+		for (int i = 0; i < 10; i++) {
+			Instantiate (Bloques[3], new Vector3(0,0,dEntreBloques), Quaternion.identity);
+			dEntreBloques+=7.6175f;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (escenaZ < 100) {
+			for (int i = 0; i < 5; i++) {
+				Instantiate (Bloques [3], new Vector3 (0, 0, dEntreBloques), Quaternion.identity);
+				dEntreBloques += 7.6175f;
+			}	
+		}
 	}
 }
