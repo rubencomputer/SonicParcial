c3fb38e3079273ea27f36d82ea499eb4915ac4ba
mrdanielfran <danielfran1113@icloud.com>
2017-10-20 19:29:15 -0500

    Guardado de datos 2
    Codigo Comentado

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,19 +12,22 @@ public class GameManager : MonoBehaviour {
 
 
 	//VARIABLES A MODIFICAR MIENTRAS SE JUEGA
-	public static int coins = 1999; 
-	public static int distancia = 1000000;
-	public static int enemigos = 1;
-	public static int score = 1;
+	public static int coins = 0; 
+	public static int distancia = 0;
+	public static int enemigos = 0;
+	public static int score = 0;
 
 	void Update () 
 	{
-		if (Input.GetKeyDown (KeyCode.Space)) 
+		if (Input.GetKeyDown (KeyCode.C)) 
 		{
 			Cargar ();
 			InicializaDatos ();
 
 		}
+
+		if (Input.GetKeyDown (KeyCode.G)) 
+			Guardar ();
 
 	}
 	
@@ -43,18 +46,25 @@ public class GameManager : MonoBehaviour {
 	public static void Guardar()
 	{
 
+		//Por el momento solo guarda 4 variables, 
+		//en caso de querer guardar mas
+		//1-Crear una variable en la clase SaveData Ej: public int edad
+		//2-Crear otra variable en esta instancia Ej:  public static int _edad (Esta variable es la que se va a modificar durante el juego)
+		//3-Finalmente igualarlas en esta funcion EJ:  Progreso.edad = _edad;
 		Progreso.topCoins = coins;
 		Progreso.topDistancia = distancia;
 		Progreso.topEnemigos = enemigos;
 		Progreso.topScore = score;
 
-		print (Application.persistentDataPath);
-		string ArchivoJson = JsonUtility.ToJson (Progreso);
-		File.WriteAllText (Application.persistentDataPath + "Progreso.json", ArchivoJson);
+		print (Application.persistentDataPath); //Imprime la ruta donde se guarda el JSON
+		string ArchivoJson = JsonUtility.ToJson (Progreso); //Convierte toda nuestra clase SaveData a un string 
+		File.WriteAllText (Application.persistentDataPath + "Progreso.json", ArchivoJson); //Genera el archivo JSON con el string ArchivoJson en el directorio.
 		Debug.Log (ArchivoJson);
 
-		if(ArchivoJson.Length > 0 && ArchivoJson != null) //Se asegura de que se haya creado el archivo JSON
+		if (ArchivoJson.Length > 0 && ArchivoJson != null) //Se asegura de que se haya creado el archivo JSON
 			print ("Se ha guardado la partida");
+		else
+			print ("Error al guardar");
 		
 	}
 
@@ -62,7 +72,11 @@ public class GameManager : MonoBehaviour {
 	 DEL JSON ANTERIORMENTE CONVERTIDO EN OBJETO */
 	public static void InicializaDatos()
 	{
-		//Como el archivo es un JSON (String), hago la conversion de string al tipo de variable que necesitamos
+		//Como el archivo es un JSON se convirtiío en objeto en la variable itemData, ahora para cargar el valor que queramos
+		// solo hay que hacer la conversion especificandole la posicion (ya que el json es un arreglo) le indicamos itemData["nombreDelaVariable"].ToString()
+		//y el out es donde se va a asignar ese valor.
+
+		//Tipo de dato         NombreVariable a extraer   Nombre variable a asignar ese dato
 		int.TryParse(itemData["topScore"].ToString(), out score) ;
 		int.TryParse(itemData["topEnemigos"].ToString(), out enemigos) ;
 		int.TryParse(itemData["topDistancia"].ToString(), out distancia) ;
--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -7,6 +7,7 @@ public class SaveData {
 
 	//Variables utilizadas para alojar los datos que se van a guardar
 	//Posteriormente se utilizan en la funcion Guardar();
+	//ESTAS VARIABLES SON LAS QUE SE ESCRIBEN EN EL ARCHIVO JSON
 	public int topScore;
 	public int topCoins;
 	public int topEnemigos;
