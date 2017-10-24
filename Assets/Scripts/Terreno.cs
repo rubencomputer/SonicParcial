c1ee30aac9c117d410a133225e33410e9f975f68
mrdanielfran <danielfran1113@icloud.com>
2017-10-24 13:59:48 -0500

    Arreglo de arreglos DONE
    porfin

--- Assets/Scripts/Terreno.cs
+++ Assets/Scripts/Terreno.cs
@@ -5,31 +5,33 @@ using UnityEngine;
 
 
 public class Terreno : MonoBehaviour {
-	
-	public GameObject[] Bloques;
-	public GameObject[] Puentes;
-	public GameObject[] Puenques;
-	public GameObject Vacio;
-	public GameObject[] Obstaculos;
-	public GameObject[] Items;
+	[System.Serializable]
+	public class Obstaculos2
+	{
+		public string tipoObjeto;
+		public GameObject[] obs;
+	}
+
+	public Obstaculos2[] Todo;
+
 	private float dEntreBloques = 7.6175f;
 	private float escenaZ = 83.7925f;
 
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 10; i++) {
+		/*for (int i = 0; i < 10; i++) {
 			Instantiate (Bloques[3], new Vector3(0,0,dEntreBloques), Quaternion.identity);
 			dEntreBloques+=7.6175f;
-		}
+		}*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (escenaZ < 100) {
+		/*if (escenaZ < 100) {
 			for (int i = 0; i < 5; i++) {
 				Instantiate (Bloques [3], new Vector3 (0, 0, dEntreBloques), Quaternion.identity);
 				dEntreBloques += 7.6175f;
 			}	
-		}
+		}*/
 	}
 }
