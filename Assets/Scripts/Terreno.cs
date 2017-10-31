f55eb9bc6c54dd0d54177ba9fd9a301e69a995e2
mrdanielfran <danielfran1113@icloud.com>
2017-10-31 15:26:24 -0600

    REMAKE PARA POOLING Y SPAWN DE ITEMS
    salvame Dios

--- Assets/Scripts/Terreno.cs
+++ Assets/Scripts/Terreno.cs
@@ -13,28 +13,97 @@ public class Terreno : MonoBehaviour {
 	}
 
 	public Obstaculos2[] Todo;
+	public static List<GameObject> pool = new List<GameObject> ();
 
-	public Transform jugador;
-	private float dEntreBloques = 7.6175f;
+	public  List<GameObject> poolCopia = new List<GameObject> ();
+
+	public List<Transform> posItem = new List<Transform>();
+
+	public GameObject objetos;
+	public GameObject spawnItem;
+	public static Transform jugador;
+	public static float dEntreBloques = 0f;
 	private float startZ = 83.7925f;
 	private float spawnLeft = -4.0f;
+	private float spawnMiddle = 0.0f;
 	private float spawnRight = 4.0f;
 	private float spawndist = 100.0f;
 	private int randomTipo;
 	private int randomPosicion;
+	private int randomItem;
+	private int randomItemSelect;
+	public GameObject suelo;
 	// Use this for initialization
 	void Start () {
-		Instantiate (Todo[0].obs[0], new Vector3 (0, 0, dEntreBloques), Quaternion.identity);
+
+		pool.Add (suelo); 
+		pool.Add (suelo); 
+		pool.Add (suelo);
+		pool.Add (suelo);
+		poolCopia = pool;
+
+		jugador = GameObject.FindWithTag ("Player").transform;
+
+		ColectaTransforms ();
+
 	}
-	
 	// Update is called once per frame
 	void Update () {
-		randomTipo = Random.Range (0, 3);
-		randomPosicion = Random.Range (0, 5);
+		randomTipo = Random.Range (0, 2);
+		randomItem = Random.Range (1, 75);
+		randomItemSelect = Random.Range (1, 100);
+		if (randomItemSelect == 30) {
+			Instantiate (spawnItem, posItem [randomItem].transform.position, Quaternion.identity);
+		}
 	}
 	void OnTriggerEnter(Collider _col)
 	{
-		Instantiate(Todo[randomTipo].obs[randomPosicion], new Vector3 (0,0,236.1425+dEntreBloques),Quaternion.identity);
-		dEntreBloques += 7.6175;
+		
+		if (_col.gameObject.CompareTag("Untagged"))
+		{
+			print("Colision con pared");
+			GenerarSuelo ();
+			_col.gameObject.GetComponent<BoxCollider> ().enabled = false;
+
+			_col.gameObject.SetActive (false);
+			/*
+			Instantiate(Todo[0].obs[randomTipo], new Vector3 (0,0,236.1425f+dEntreBloques),Quaternion.identity, objetos);
+			dEntreBloques += 114.2625f;
+			Destroy (_col.gameObject, 3.0f);
+*/
+		}
+	}
+
+	public static GameObject GenerarSuelo()
+	{
+		for (int i = 0; i < pool.Count; i++)
+		{
+			if (pool [i].activeSelf == false)
+			{
+				pool [i].SetActive (true);
+				pool [i].transform.position = new Vector3 (0, 0, jugador.position.z);
+				pool [i].GetComponent<BoxCollider> ().enabled = true;
+				return pool [i];
+			}
+		}
+
+	//	GameObject nuevoSuelo = Instantiate(Todo[0].obs[randomTipo], new Vector3 (0,0,236.1425f+dEntreBloques),Quaternion.identity);
+		GameObject nuevoSuelo = Instantiate(pool[1], new Vector3 (0,0,114.2625f),Quaternion.identity) as GameObject;
+
+		pool.Add (nuevoSuelo);
+		return nuevoSuelo;
+	}
+
+
+	public void ColectaTransforms()
+	{
+		for (int i = 0; i < suelo.transform.childCount; i++)
+		{
+			posItem.Add(suelo.transform.GetChild(i).transform.GetChild(0).GetChild(1).transform);
+			posItem.Add(suelo.transform.GetChild(i).transform.GetChild(0).GetChild(2).transform);
+			posItem.Add(suelo.transform.GetChild(i).transform.GetChild(0).GetChild(3).transform);
+						
+					
+		}
 	}
 }
